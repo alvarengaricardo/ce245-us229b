968b9f19861dda8c16e0862e0aea13c93a11cc73
ricardo <alvarenga.r.ricardo@gmail.com>
2018-05-02 01:38:42 -0300

    crud_leitos

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var Produto = require('./app/models/produto');
+var Leito = require('./app/models/models');
 
 //URI: mlab
-mongoose.connect('mongodb://usuario:camboja@ds014118.mlab.com:14118/node-crud-api');
+mongoose.connect('mongodb://usuario:senha@ds014648.mlab.com:14648/hospital');
 
 // Configuração da variável app para usar o 'bodyParser()':
 app.use(bodyParser.urlencoded({extended: true}));
@@ -45,85 +45,89 @@ router.get('/', function(req, res){
 //API's:
 //==============================================================================
 
-//Rotas que terminarem com '/produtos' (servir: GET ALL & POST)
-router.route('/produtos')
+//Rotas que terminarem com '/leitos' (servir: GET ALL & POST)
+router.route('/leitos')
 
-    /* 1) Método: Criar Produto (acessar em: POST http://localhost:8000/api/produtos)  */
-    .post(function(req, res) {
-        var produto = new Produto();
+    /* 1) Método: Criar Leito (acessar em: POST http://localhost:8000/api/leitos)  */
+    .post(function(req, res) {        
+        var leito = new Leito(); 
+        //setar os campos do leito (via request):
+        leito.hospital = req.body.hospital;
+        leito.status = req.body.status;
+        leito.setor = req.body.setor;
+        leito.andar = req.body.andar;
+        leito.ala = req.body.ala;
+        leito.tipo = req.body.tipo;
 
-        //Aqui vamos setar os campos do produto (via request):
-        produto.nome = req.body.nome;
-        produto.preco = req.body.preco;
-        produto.descricao = req.body.descricao;
 
-        produto.save(function(error) {
+        leito.save(function(error) {
             if(error)
-                res.send('Erro ao tentar salvar o Produto....: ' + error);
+                res.send('Erro ao tentar salvar o leito....: ' + error);
             
-            res.json({ message: 'Produto Cadastrado com Sucesso!' });
+            res.json({ message: 'Leito Cadastrado com Sucesso!' });
         });
     })
 
-    /* 2) Método: Selecionar Todos Produtos (acessar em: GET http://localhost:8000/api/produtos)  */
+    /* 2) Método: Selecionar Todos leitos (acessar em: GET http://localhost:8000/api/leitos)  */
     .get(function(req, res) {
-        Produto.find(function(error, produtos) {
+        Leito.find(function(error, leitos) {
             if(error) 
-                res.send('Erro ao tentar Selecionar Todos os produtos...: ' + error);
-
-            res.json(produtos);
+                res.send('Erro ao tentar Selecionar todos os leitos...: ' + error);
+            res.json(leitos);
         });
     });
 
-    //Rotas que irão terminar em '/produtos/:produto_id' (servir tanto para: GET, PUT & DELETE: id):
-    router.route('/produtos/:produto_id')
+    //Rotas que irão terminar em '/leitos/:leito_id' (servir tanto para: GET, PUT & DELETE: id):
+    router.route('/leitos/:leito_id')
 
-    /* 3) Método: Selecionar por Id: (acessar em: GET http://localhost:8000/api/produtos/:produto_id) */
+    /* 3) Método: Selecionar por Id: (acessar em: GET http://localhost:8000/api/leitos/leito_id) */
     .get(function (req, res) {
         
-        //Função para poder Selecionar um determinado produto por ID - irá verificar se caso não encontrar um detemrinado
-        //produto pelo id... retorna uma mensagem de error:
-        Produto.findById(req.params.produto_id, function(error, produto) {
+        //Função para poder Selecionar um determinado leito por ID - irá verificar se caso não encontrar um deteminado
+        //leito pelo id... retorna uma mensagem de error:
+        Leito.findById(req.params.leito_id, function(error, leito) {
             if(error)
-                res.send('Id do Produto não encontrado....: ' + error);
+                res.send('Id do leito não encontrado....: ' + error);
 
-            res.json(produto);
+            res.json(leito);
         });
     })
 
-    /* 4) Método: Atualizar por Id: (acessar em: PUT http://localhost:8000/api/produtos/:produto_id) */
+    /* 4) Método: Atualizar por Id: (acessar em: PUT http://localhost:8000/api/leitos/:leito_id) */
     .put(function(req, res) {
 
-        //Primeiro: para atualizarmos, precisamos primeiro achar 'Id' do 'Produto':
-        Produto.findById(req.params.produto_id, function(error, produto) {
+        //Primeiro: para atualizarmos, precisamos primeiro achar 'Id' do 'leito':
+        Leito.findById(req.params.leito_id, function(error, leito) {
             if (error) 
-                res.send("Id do Produto não encontrado....: " + error);
+                res.send("Id do leito não encontrado....: " + error);
 
                 //Segundo: 
-                produto.nome = req.body.nome;
-                produto.preco = req.body.preco;
-                produto.descricao = req.body.descricao;
+                leito.hospital = req.body.hospital;
+                leito.status = req.body.status;
+                leito.setor = req.body.setor;
+                leito.andar = req.body.andar;
+                leito.ala = req.body.ala;
+                leito.tipo = req.body.tipo;
 
-                //Terceiro: Agora que já atualizamos os dados, vamos salvar as propriedades:
-                produto.save(function(error) {
+                leito.save(function(error) {
                     if(error)
-                        res.send('Erro ao atualizar o produto....: ' + error);
+                        res.send('Erro ao atualizar o leito....: ' + error);
 
-                    res.json({ message: 'Produto atualizado com sucesso!' });
+                    res.json({ message: 'leito atualizado com sucesso!' });
                 });
             });
         })
 
-        /* 5) Método: Excluir por Id (acessar: http://localhost:8000/api/produtos/:produto_id) */
+        /* 5) Método: Excluir por Id (acessar: http://localhost:8000/api/leitos/:leito_id) */
         .delete(function(req, res) {
             
-            Produto.remove({
-                _id: req.params.produto_id
+            Leito.remove({
+                _id: req.params.leito_id
                 }, function(error) {
                     if (error) 
-                        res.send("Id do Produto não encontrado....: " + error);
+                        res.send("Id do leito não encontrado....: " + error);
 
-                    res.json({ message: 'Produto Excluído com Sucesso!' });
+                    res.json({ message: 'Leito excluído com sucesso!' });
                 });
             });
 
